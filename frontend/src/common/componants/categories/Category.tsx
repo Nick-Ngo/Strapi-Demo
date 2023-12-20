8134e743be0eae056f285576843b722d216f55ad
Ngo Trung Hieu <hieu.n2395@gmail.com>
2023-12-21 00:08:33 +0700

    source branch 2

--- frontend/src/common/componants/categories/Category.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-const Category = ({ cat }: any) => {
-    return (
-        <div onClick={() => { }} className="bg-[#af8533] p-4 rounded-lg shadow-md cursor-pointer">{cat?.attributes?.Title}</div>
-    );
-};
-
-export default Category;
